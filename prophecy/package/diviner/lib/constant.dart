f98f81d1436638ec5bacb7ae4c63fc1ed0c0b0f5
Bruno DE OLIVEIRA <bruno.de-oliveira@epitech.eu>
2022-05-26 13:50:54 +0000

    feat(diviner): Add update(password/username/email address) && delete account routes

--- prophecy/package/diviner/lib/constant.dart
+++ prophecy/package/diviner/lib/constant.dart
@@ -4,4 +4,8 @@ class AccountEndPoints {
   static String signIn = '/account/sign-in';
   static String signUp = '/account/sign-up';
   static String signOut = '/account/sign-out';
+  static String updatePassword = '/account/settings/update-password';
+  static String updateUsername = '/account/settings/update-username';
+  static String updateEmailAddress = '/account/settings/update-email-address';
+  static String deleteAccount = '/account/settings/delete-account';
 }
