719b99d14f5dd32e1e898718e023fccce762e989
Bruno DE OLIVEIRA <bruno.de-oliveira@epitech.eu>
2022-05-26 16:12:56 +0000

    feat(diviner): Add Events routes

--- prophecy/package/diviner/lib/src/service/event.dart
+++ prophecy/package/diviner/lib/src/service/event.dart
@@ -23,6 +23,40 @@ class CreateEventService {
   }
 }
 
+class UpdateEventService {
+  Future<int?> saveEvent(
+      String bearerToken, String eventId, Event event) async {
+    try {
+      var url = Uri.parse(baseUrl + EventEndPoints.updateEvent + eventId);
+      var response = await http.put(url,
+          headers: {'Authorization': 'Bearer $bearerToken'},
+          body: event.toJson());
+      if (response.statusCode == 200) {
+        return response.statusCode;
+      }
+    } catch (e) {
+      log(e.toString());
+    }
+    return null;
+  }
+}
+
+class DeleteEventService {
+  Future<int?> saveEvent(String bearerToken, String eventId) async {
+    try {
+      var url = Uri.parse(baseUrl + EventEndPoints.deleteEvent + eventId);
+      var response = await http
+          .delete(url, headers: {'Authorization': 'Bearer $bearerToken'});
+      if (response.statusCode == 200) {
+        return response.statusCode;
+      }
+    } catch (e) {
+      log(e.toString());
+    }
+    return null;
+  }
+}
+
 class SaveEventService {
   Future<int?> saveEvent(String bearerToken, String eventId) async {
     try {
@@ -31,6 +65,41 @@ class SaveEventService {
           .put(url, headers: {'Authorization': 'Bearer $bearerToken'});
       if (response.statusCode == 200) {
         return response.statusCode;
+      }
+    } catch (e) {
+      log(e.toString());
+    }
+    return null;
+  }
+}
+
+class UnsaveEventService {
+  Future<int?> saveEvent(String bearerToken, String eventId) async {
+    try {
+      var url = Uri.parse(baseUrl + EventEndPoints.unsaveEvent + eventId);
+      var response = await http
+          .put(url, headers: {'Authorization': 'Bearer $bearerToken'});
+      if (response.statusCode == 200) {
+        return response.statusCode;
+      }
+    } catch (e) {
+      log(e.toString());
+    }
+    return null;
+  }
+}
+
+class GetEventService {
+  Future<Event?> getEvent(String bearerToken, String eventId) async {
+    try {
+      var url = Uri.parse(baseUrl + EventEndPoints.getEvent + eventId);
+      var response = await http.get(
+        url,
+        headers: {'Authorization': 'Bearer $bearerToken'},
+      );
+      if (response.statusCode == 200) {
+        Event _model = eventFromJson(response.body);
+        return _model;
       }
     } catch (e) {
       log(e.toString());
@@ -49,6 +118,32 @@ class GetEventsService {
               ? '?city=$city'
               : '?city=$city&locationId=$locationId');
       var url = Uri.parse(baseUrl + EventEndPoints.getEvents + query);
+      var response = await http.get(
+        url,
+        headers: {'Authorization': 'Bearer $bearerToken'},
+      );
+      if (response.statusCode == 200) {
+        List<String> _events =
+            List<String>.from(json.decode(response.body).map((x) => x));
+        return _events;
+      }
+    } catch (e) {
+      log(e.toString());
+    }
+    return null;
+  }
+}
+
+class GetSavedEventsService {
+  Future<List<String>?> getSavedEvents(String bearerToken,
+      {String city = '', String locationId = ''}) async {
+    try {
+      var query = city.isEmpty
+          ? (locationId.isEmpty ? '' : '?locationId=$locationId')
+          : (locationId.isEmpty
+              ? '?city=$city'
+              : '?city=$city&locationId=$locationId');
+      var url = Uri.parse(baseUrl + EventEndPoints.getSavedEvents + query);
       var response = await http.get(
         url,
         headers: {'Authorization': 'Bearer $bearerToken'},
